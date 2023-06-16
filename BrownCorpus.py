7da43620083ff5ad14f3aaf2a9e32f6af4ee7b33
Mykel Brinkerhoff <mlbrinke@ucsc.edu>
2023-06-16 16:00:43 -0700

    adding stuff to the output

--- BrownCorpus.py
+++ BrownCorpus.py
@@ -12,19 +12,37 @@ from nltk.corpus import brown
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist
-from nltk.corpus import reuters
+from nltk.corpus import cmudict
 import csv
 import string
 
-# Download the Brown Corpus and stopwords
+# Download the Brown Corpus, stopwords, POS tagger, and pronunciation dictionary
 nltk.download('brown')
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('cmudict')
 
 # Load the Brown Corpus
 sentences = brown.sents()
 
-# Retrieve the first word of each sentence
-first_words = [sentence[0] for sentence in sentences]
+# Initialize the POS tagger and pronunciation dictionary
+pos_tagger = nltk.pos_tag
+pronunciation_dict = cmudict.dict()
+
+# Retrieve the first word, POS tag, and pronunciation of each sentence
+first_words = []
+pos_tags = []
+pronunciations = []
+
+for sentence in sentences:
+    if len(sentence) > 0:
+        first_word = sentence[0]
+        pos_tag = pos_tagger([first_word])[0][1]
+        pronunciation = pronunciation_dict.get(first_word.lower(), [[]])[0]
+
+        first_words.append(first_word)
+        pos_tags.append(pos_tag)
+        pronunciations.append(pronunciation)
 
 # Tokenize and remove punctuation
 translator = str.maketrans('', '', string.punctuation)
@@ -34,14 +52,17 @@ first_words = [word.translate(translator) for word in first_words]
 frequency = FreqDist(first_words)
 
 # Remove stopwords
-# stopwords = set(stopwords.words('english'))
+stopwords = set(stopwords.words('english'))
 frequency = {word: count for word, count in frequency.items() if word.lower() not in stopwords}
 
 # Export the result as a CSV file
-output_file = 'first_word_frequency.csv'
+output_file = 'first_word_info.csv'
 with open(output_file, 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['First Word', 'Frequency'])
-    writer.writerows(frequency.items())
+    writer.writerow(['First Word', 'POS', 'Pronunciation', 'Frequency'])
+    for word, count in frequency.items():
+        pos_tag = pos_tags[first_words.index(word)]
+        pronunciation = " ".join(pronunciations[first_words.index(word)])
+        writer.writerow([word, pos_tag, pronunciation, count])
 
 print(f"The results have been exported to {output_file}.")
